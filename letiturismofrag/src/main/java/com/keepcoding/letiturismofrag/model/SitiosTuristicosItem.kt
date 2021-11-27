17c1993b697165f27da92b9a53c755e8daca56ad
MrLMen09 <lemendozat@correo.udistrital.edu.co>
2021-11-27 00:18:09 -0500

    Fragmentos

---
+++ letiturismofrag/src/main/java/com/keepcoding/letiturismofrag/model/SitiosTuristicosItem.kt
@@ -0,0 +1,28 @@
+package com.keepcoding.letiturismofrag.model
+
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class SitiosTuristicosItem(
+    @SerializedName("datosAdicionales")
+    val datosAdicionales: String,
+    @SerializedName("descripcion")
+    val descripcion: String,
+    @SerializedName("descripcionCorta")
+    val descripcionCorta: String,
+    @SerializedName("horario")
+    val horario: String,
+    @SerializedName("nombre")
+    val nombre: String,
+    @SerializedName("precio")
+    val precio: String,
+    @SerializedName("puntuacion")
+    val puntuacion: Double,
+    @SerializedName("temperatura")
+    val temperatura: String,
+    @SerializedName("ubicacion")
+    val ubicacion: String,
+    @SerializedName("UrlImagen")
+    val urlImagen: String
+) : Serializable
