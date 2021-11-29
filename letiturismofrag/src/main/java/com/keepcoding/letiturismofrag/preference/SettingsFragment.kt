072153763baaf2bbe9195dff834d564dd94e7f37
Alice403 <alejandra403.arciniegas@hotmail.es>
2021-11-28 23:05:32 -0500

    aplicación completa con menú

--- letiturismofrag/src/main/java/com/keepcoding/letiturismofrag/preference/SettingsFragment.kt
+++ letiturismofrag/src/main/java/com/keepcoding/letiturismofrag/preference/SettingsFragment.kt
@@ -1,12 +1,26 @@
 package com.keepcoding.letiturismofrag.preference
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.preference.PreferenceFragmentCompat
 import com.keepcoding.letiturismofrag.R
+import com.keepcoding.letiturismofrag.main.MainActivity
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        (activity as MainActivity)?.showIcon()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
     }
 }
